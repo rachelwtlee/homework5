87d8fd49f651e1f4bd1689fbfb1da9518bf2e0da
rachelwtlee <rachelwtlee@gmail.com>
2020-10-25 16:26:19 +0800

    Initial commit homework 6

---
+++ homework_6/js/script.js
@@ -0,0 +1,50 @@
+//BUTTON HOVER STYLING
+function buttonHover() {
+    document.getElementById("pumpkin").style.background = "#F68838";
+    document.getElementById("pumpkin").style.color = "#FFF";
+    document.getElementById("pumpkin").style.border = "1px solid white";
+    document.getElementById("pumpkin").style.cursor = "pointer";
+}
+
+function buttonOut() {
+    document.getElementById("pumpkin").style.background = "";
+    document.getElementById("pumpkin").style.color = "";
+    document.getElementById("pumpkin").style.border = "";
+    document.getElementById("pumpkin").style.cursor = "";
+}
+
+function buttonHover2() {
+    document.getElementById("caramel").style.background = "#F68838";
+    document.getElementById("caramel").style.color = "#FFF";
+    document.getElementById("caramel").style.border = "1px solid white";
+    document.getElementById("caramel").style.cursor = "pointer";
+}
+
+function buttonOut2() {
+    document.getElementById("caramel").style.background = "";
+    document.getElementById("caramel").style.color = "";
+    document.getElementById("caramel").style.border = "";
+    document.getElementById("caramel").style.cursor = "";
+}
+
+function buttonHover3() {
+    document.getElementById("walnut").style.background = "#F68838";
+    document.getElementById("walnut").style.color = "#FFF";
+    document.getElementById("walnut").style.border = "1px solid white";
+    document.getElementById("walnut").style.cursor = "pointer";
+}
+
+function buttonOut3() {
+    document.getElementById("walnut").style.background = "";
+    document.getElementById("walnut").style.color = "";
+    document.getElementById("walnut").style.border = "";
+    document.getElementById("walnut").style.cursor = "";
+}
+
+function primaryHover() {
+    document.getElementById("primary").style.cursor = "pointer";
+}
+
+function primaryOut() {
+    document.getElementById("primary").style.cursor = "pointer";
+}
---
+++ homework_6/readme.txt
@@ -0,0 +1,3 @@
+EXTRA CREDIT
+
+I created 2 additional pages— an about page (about.html) and a contact page (contact.html)
